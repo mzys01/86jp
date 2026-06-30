d0091e664bebfcb6d9c9c302880ba6559b526549
L1Bai <l1bai@noreply.codeberg.org>
2026-06-30 11:01:57 +0200

    兑换券支付和属性选择现在能一路从客户端请求穿透到最底层的数据库操作
    加了 paymentMode 和 attributeValue 两个参数

--- Server/DfoServer/Game/Inventory/SqliteInventoryStore.cs
+++ Server/DfoServer/Game/Inventory/SqliteInventoryStore.cs
@@ -2356,12 +2356,14 @@ WHERE character_id = @cid AND slot = @slot AND item_id = @itemId;";
             return requestedCount;
         }
 
-        public bool TryBuyCeraShopItem(int productId, int buyCount, out InventoryMutationResult result)
+        // 商城购买入口: 开启事务, 透传 paymentMode 与 attributeValue 到 InventoryShopStore。
+        // 事务在方法内开启, 成功则 Commit, 失败自动回滚(Dispose 不 Commit)。
+        public bool TryBuyCeraShopItem(int productId, int buyCount, int paymentMode, byte attributeValue, out InventoryMutationResult result)
         {
             using (var connection = _context.OpenConnection())
             using (var transaction = connection.BeginTransaction())
             {
-                var ok = _shopStore.TryBuyCeraShopItem(connection, transaction, _context.CharacterId, _context.AccountId, productId, buyCount, out result);
+                var ok = _shopStore.TryBuyCeraShopItem(connection, transaction, _context.CharacterId, _context.AccountId, productId, buyCount, paymentMode, attributeValue, out result);
                 if (ok) transaction.Commit();
                 return ok;
             }
