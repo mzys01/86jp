10349047fe679dd1469283ad71c835a4ab6b3e4e
Agni_Shine <shineagni@gmail.com>
2026-07-02 14:16:27 +0800

    修改游戏设置/键盘设置为选角色前下发，修正设置不生效问题

--- Server/DfoServer/Network/Builders/Init/GameOptionBodyBuilder.cs
+++ Server/DfoServer/Network/Builders/Init/GameOptionBodyBuilder.cs
@@ -16,21 +16,7 @@ namespace DfoServer.Network.Builders
             var bank0 = init.QuickchatBank0 ?? Array.Empty<byte>();
             var bank1 = init.QuickchatBank1 ?? Array.Empty<byte>();
 
-            var writer = new Network.GamePacketWriter();
-
-            writer.WriteInt32(main.Length);
-            for (int i = 0; i < main.Length; i++)
-                writer.WriteByte(main[i]);
-
-            writer.WriteInt32(bank0.Length);
-            for (int i = 0; i < bank0.Length; i++)
-                writer.WriteByte(bank0[i]);
-
-            writer.WriteInt32(bank1.Length);
-            for (int i = 0; i < bank1.Length; i++)
-                writer.WriteByte(bank1[i]);
-
-            body = writer.ToArray();
+            body = AccountSettingsPacketBuilder.BuildGameOptionBody(main, bank0, bank1);
             return true;
         }
     }
--- Server/DfoServer/Network/Builders/Init/HotkeyConfigBodyBuilder.cs
+++ Server/DfoServer/Network/Builders/Init/HotkeyConfigBodyBuilder.cs
@@ -9,14 +9,8 @@ namespace DfoServer.Network.Builders
 
         public bool TryBuild(SelectCharacterDataSnapshot snapshot, int occurrenceIndex, out byte[] body)
         {
-            var slots = snapshot.InitializationSnapshot.HotkeyConfigSlots;
-            var slotCount = slots?.Count ?? 0;
-            var blobLen = slotCount * 2;
-            body = new byte[1 + 4 + blobLen];
-            body[0] = snapshot.InitializationSnapshot.HotkeyKeyType;
-            Buffer.BlockCopy(BitConverter.GetBytes(blobLen), 0, body, 1, 4);
-            for (var i = 0; i < slotCount; i++)
-                Buffer.BlockCopy(BitConverter.GetBytes(slots[i]), 0, body, 5 + i * 2, 2);
+            var init = snapshot.InitializationSnapshot;
+            body = AccountSettingsPacketBuilder.BuildHotkeyOptionBody(init.HotkeyKeyType, init.HotkeyConfigSlots);
             return true;
         }
     }
